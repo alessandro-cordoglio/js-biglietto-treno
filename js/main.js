6e0b98b837fc567f588fea03121fc66b0f30f387
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-20 14:08:58 +0200

    Termine esercizio

--- js/main.js
+++ js/main.js
@@ -6,19 +6,17 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
 
 /* creo le variabili */
-
-
 const journey_km = Number(prompt("Inserisci i km che vorresti percorrere"));
 const user_age = Number(prompt("Inserisci la tua età"));
 
 let ticket_price = 0.21*journey_km;
 
+/* condizione che crea il prezzo scontato */
 if(user_age<18){
     ticket_price = ticket_price - ((ticket_price*20)/100);
 }
 else if(user_age>=65){
     ticket_price = ticket_price - ((ticket_price*40)/100);
 }
-else{
 
-}
+document.getElementById("ticket_price").innerHTML=`Il prezzo del tuo biglietto è ${ticket_price.toFixed(2)}`
